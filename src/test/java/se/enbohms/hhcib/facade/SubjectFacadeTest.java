b771c4b6a0d76a21a61fc0e564ba148ca2ba1e20
enbohm <andreas@enbohms.se>
2013-08-14 08:57:05 +0200

    Added tests

--- src/test/java/se/enbohms/hhcib/facade/SubjectFacadeTest.java
+++ src/test/java/se/enbohms/hhcib/facade/SubjectFacadeTest.java
@@ -1,12 +1,23 @@
-package enbohms.hhcib.facade;
+package se.enbohms.hhcib.facade;
+
+import java.util.List;
 
 import org.junit.Test;
 
+import se.enbohms.hhcib.entity.Category;
+import se.enbohms.hhcib.entity.Subject;
+
 public class SubjectFacadeTest {
 
-	
 	@Test
 	public void should_return_list_with_subjects_for_economy() throws Exception {
-
+		//given
+		SubjectFacade facade = new SubjectFacade();
+		
+		//when
+		List<Subject> result = facade.getSubjectFor(Category.ECONOMY);
+		
+		//then
+		
 	}
 }
