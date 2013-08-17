85aaad82a4734ace9a6621234bd44a09e5c220aa
enbohm <andreas@enbohms.se>
2013-08-17 16:23:25 +0200

    minor templating refactoring

--- src/main/java/se/enbohms/hhcib/facade/HowToFacade.java
+++ src/main/java/se/enbohms/hhcib/facade/HowToFacade.java
@@ -2,32 +2,27 @@ package se.enbohms.hhcib.facade;
 
 import java.io.Serializable;
 
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import se.enbohms.hhcib.entity.Category;
 import se.enbohms.hhcib.service.api.CrudService;
 
 @Named
-@ViewScoped
+@RequestScoped
 public class HowToFacade implements Serializable {
 
 	private static final long serialVersionUID = -3633333461394775021L;
 
+	@NotNull
+	@Size(min = 10)
 	private String description;
-	private String category;
 
 	@Inject
 	private CrudService service;
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
 
 	public String getDescription() {
 		return description;
@@ -37,10 +32,9 @@ public class HowToFacade implements Serializable {
 		this.description = description;
 	}
 
-	public String save() {
+	public void save() {
 		System.out.println("submitted value  " + description);
 		service.createSubject("a heading", description, Category.FOOD);
-		return "/hhcib/index.xhtml";
 	}
 
 	public void clear() {
