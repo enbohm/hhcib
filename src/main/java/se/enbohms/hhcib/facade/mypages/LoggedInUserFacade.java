5b8e0ad179c039e5f8eae9aae1aefc510c46d78f
enbohm <andreas@enbohms.se>
2013-09-09 09:57:29 +0200

    Added LoggedInUserFacade class

---
+++ src/main/java/se/enbohms/hhcib/facade/mypages/LoggedInUserFacade.java
@@ -0,0 +1,29 @@
+package se.enbohms.hhcib.facade.mypages;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+import se.enbohms.hhcib.common.Constants;
+import se.enbohms.hhcib.entity.User;
+
+/**
+ * JSF Facade handling user login scenario
+ */
+@Named
+@RequestScoped
+public class LoggedInUserFacade {
+
+	/**
+	 * 
+	 * @return the logged in user fetched from the {@link HttpSession}
+	 */
+	public User getLoggedInUser() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) context.getExternalContext()
+				.getSession(false);
+		return (User) session.getAttribute(Constants.USER);
+	}
+
+}
